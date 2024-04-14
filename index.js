06a720ebc619099d27f1ded4fa3a7e55a9345927
vasilyocheck <138142439+vasilyocheck@users.noreply.github.com>
2024-04-14 21:03:31 +0300

    temp save

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 import express from 'express';
+import jwt from "jsonwebtoken";
 const app = express();
+import dotenv from "dotenv";
+dotenv.config();
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Welcome to Joints API!');
+})
+
+app.post('/auth/login', (req, res) => {
+    const token = jwt.sign({
+        email: req.body.email,
+        fullName: req.body.fullName,
+    }, process.env.SECRET)
+    res.send({
+        success: true,
+        token
+    })
 })
 
 app.listen(4444, (e) => {
