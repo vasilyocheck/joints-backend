8fd1a79f4b60ea11a406c2e851ee3e3bed189ddd
vasilyocheck <vasily.medvedev1986@gmail.com>
2024-04-21 01:29:21 +0300

    allow credentials

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ app.use(function (req, res, next) {
 
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Allow-Control-Allow-Credentials', 'true');
     next();
 });
 app.use(cors({
