13b05c8d4ef65bc93ca00fcfd640b8f0bc8e057e
vasilyocheck <vasily.medvedev1986@gmail.com>
2024-04-20 18:57:54 +0300

    return cors

--- index.js
+++ index.js
@@ -24,12 +24,12 @@ app.use(cors({
     optionsSuccessStatus: 200
 }))
 app.use(express.json());
-/*app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Headers","*");
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
-});*/
+});
 
 app.get('/', (req, res) => {
     res.send('Welcome to Joints API!');
