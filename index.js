6f3689194980d84fd9dd68f7d771265ad19966da
vasilyocheck <vasily.medvedev1986@gmail.com>
2024-04-20 14:15:23 +0300

    add several origins

--- index.js
+++ index.js
@@ -19,7 +19,13 @@ mongoose.connect(process.env.JOINTS_DB)
 
 app.use(express.json());
 app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", 'http://localhost:5173');
+    const allowedOrigins = ['http://localhost:5173', 'https://joints-backend.vercel.app/'];
+    const origin = req.headers.origin;
+
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
