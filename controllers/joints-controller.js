c4ca3c7fbcfbb1a2c40e23e80d4d9feadc7c4fab
vasilymedvedev <vasily.medvedev1986@gmail.com>
2024-12-14 14:50:55 +0300

    add joints part

--- controllers/joints-controller.js
+++ controllers/joints-controller.js
@@ -1,35 +1,35 @@
-import JointModel from "../models/joints.js";
+import JointModel from '../models/joints.js';
 
 export const createJoint = async (req, res) => {
-    try{
-        const doc = new JointModel({
-            name: req.body.name,
-            code: req.body.code,
-            jointLength: req.body.jointLength,
-            jointWidth: req.body.jointWidth,
-            load: req.body.load,
-            height: req.body.height,
-            surface: req.body.surface,
-            isOutsideOk: req.body.isOutsideOk,
-            corner: req.body.corner,
-        })
-        const joint = await doc.save()
-        res.json(joint)
-    } catch (e) {
-        console.log(e)
-        res.status(500).json({
-            message: 'Failed to create a joint.',
-        })
-    }
-}
+  try {
+    const doc = new JointModel({
+      name: req.body.name,
+      code: req.body.code,
+      jointLength: req.body.jointLength,
+      jointWidth: req.body.jointWidth,
+      load: req.body.load,
+      height: req.body.height,
+      surface: req.body.surface,
+      isOutsideOk: req.body.isOutsideOk,
+      corner: req.body.corner,
+    });
+    const joint = await doc.save();
+    res.json(joint);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      message: 'Failed to create a joint.',
+    });
+  }
+};
 
 export const getJoints = async (req, res) => {
-    try{
-        const joints = await JointModel.find()
-        res.status(200).json(joints)
-    } catch (e) {
-        res.json({
-            message: 'No joints found.',
-        })
-    }
-}
+  try {
+    const joints = await JointModel.find();
+    res.status(200).json(joints);
+  } catch (e) {
+    res.json({
+      message: 'No joints found.',
+    });
+  }
+};
